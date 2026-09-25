144161069b1da83685f921c7eb7b5c55dbc4c54e
seththelegend640-commits <seththelegend640@gmail.com>
2026-09-25 17:08:16 +0100

    Update util.js

--- misc/util.js
+++ misc/util.js
@@ -1,5 +1,5 @@
 module.exports = {
-	xmlFail(message = "You're grounded.") {
+	xmlFail(message = "Ты сука.") {
 		return `<error><code>ERR_ASSET_404</code><message>${message}</message><text></text></error>`;
 	},
 };
